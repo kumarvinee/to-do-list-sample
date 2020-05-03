557cf4650727925a243e4cba658af1155a07f315
kumarvinee <vineetkumar1010@yahoo.in>
2020-05-03 19:09:10 +0530

    Updates

---
+++ precache-manifest.45d2859a0e7ff8cbfd3187121601a6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b545a09b8277f10291dfd5e3b2ca547b",
+    "url": "/to-do-list-sample/index.html"
+  },
+  {
+    "revision": "df44f760d8f9033e95a6",
+    "url": "/to-do-list-sample/static/css/main.55531df2.chunk.css"
+  },
+  {
+    "revision": "ee2abe186564e733b0b9",
+    "url": "/to-do-list-sample/static/js/2.9d9f6104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-sample/static/js/2.9d9f6104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df44f760d8f9033e95a6",
+    "url": "/to-do-list-sample/static/js/main.54f88b76.chunk.js"
+  },
+  {
+    "revision": "c4fa2667b50b2368f26e",
+    "url": "/to-do-list-sample/static/js/runtime-main.9c0b0ff8.js"
+  }
+]);
